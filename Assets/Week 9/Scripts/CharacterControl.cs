b1d74c5fb662090be81c95a89583958191a1e251
Jack <Maclean.Mealey@sheridancollage.ca>
2024-03-13 18:56:14 -0400

    Made it say what class you are

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMP_Text Class;
+    
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
+        
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
+            
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        
     }
-    
+
+    private void Update()
+    {
+       Class.text = SelectedVillager.ToString();
+    }
 }
